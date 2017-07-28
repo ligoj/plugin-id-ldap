a8c332ef8d9ba820246790520dc35d53aa344eb2
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-07-28 15:18:28 +0200

    Add i18n for all parameters

---
+++ src/main/resources/META-INF/resources/webjars/service/id/ldap/nls/fr/messages.js
@@ -0,0 +1,20 @@
+define({
+	'service:id:ldap:base-dn': 'Base DN',
+	'service:id:ldap:companies-dn': 'DN des sociétés',
+	'service:id:ldap:company-pattern': 'Pattern de capture de l\'identifiant de société dans un DN',
+	'service:id:ldap:department-attribute': 'Attribut de département',
+	'service:id:ldap:groups-dn': 'DN des groupes',
+	'service:id:ldap:local-id-attribute': 'Attribut d\'identifiant local',
+	'service:id:ldap:locked-attribute': 'Attribut de verrouillage',
+	'service:id:ldap:locked-value': 'Valeur de verrouillage',
+	'service:id:ldap:password': 'Mot de passe de connexion',
+	'service:id:ldap:people-class': 'Class des personnes',
+	'service:id:ldap:people-dn': 'DN des personnes',
+	'service:id:ldap:people-internal-dn': 'People internal DN',
+	'service:id:ldap:quarantine-dn': 'DN de quarantaine',
+	'service:id:ldap:referral': 'Mode referral',
+	'service:id:ldap:uid-attribute': 'Attribut UID',
+	'service:id:ldap:url': 'URL de connexion',
+	'service:id:ldap:user-dn': 'Utilisateur de connexion',
+	'service:id:uid-pattern': 'Pattern de capture de l\'identifiant d\'utilisateur dans un DN'
+});
--- src/main/resources/META-INF/resources/webjars/service/id/ldap/nls/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/id/ldap/nls/messages.js
@@ -1,1 +1,23 @@
-define({});
+define({
+	root: {
+		'service:id:ldap:base-dn': 'Base DN',
+		'service:id:ldap:companies-dn': 'Companies DN',
+		'service:id:ldap:company-pattern': 'Company pattern capture id from DN',
+		'service:id:ldap:department-attribute': 'Department attribute',
+		'service:id:ldap:groups-dn': 'Groups DN',
+		'service:id:ldap:local-id-attribute': 'Local ID attribute',
+		'service:id:ldap:locked-attribute': 'Locked attribute',
+		'service:id:ldap:locked-value': 'Locked match value',
+		'service:id:ldap:password': 'Connection password',
+		'service:id:ldap:people-class': 'People class',
+		'service:id:ldap:people-dn': 'People DN',
+		'service:id:ldap:people-internal-dn': 'People internal DN',
+		'service:id:ldap:quarantine-dn': 'Quarantine DN',
+		'service:id:ldap:referral': 'Referral mode',
+		'service:id:ldap:uid-attribute': 'UID attribute',
+		'service:id:ldap:url': 'Connection URL',
+		'service:id:ldap:user-dn': 'Connection user',
+		'service:id:uid-pattern': 'User pattern capture id from DN'
+	},
+	fr: true
+});
