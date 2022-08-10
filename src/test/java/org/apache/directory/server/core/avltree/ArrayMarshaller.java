ec8fdc74c1c0a27251758e20e59179235d46c749
Fabrice Daugan <fabrice.daugan@gmail.com>
2022-08-10 12:37:17 +0200

    Lint Java

--- src/test/java/org/apache/directory/server/core/avltree/ArrayMarshaller.java
+++ src/test/java/org/apache/directory/server/core/avltree/ArrayMarshaller.java
@@ -19,7 +19,6 @@
  */
 package org.apache.directory.server.core.avltree;
 
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -30,166 +29,130 @@ import java.util.Comparator;
 import org.apache.directory.shared.ldap.util.StringTools;
 
 /**
- * Class to serialize the Array data.
- * Only there for Java11 compliance.
+ * Class to serialize the Array data. Only there for Java11 compliance.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
  */
 @SuppressWarnings("unchecked")
-public class ArrayMarshaller<E> implements Marshaller<ArrayTree<E>>
-{
-    /** used for serialized form of an empty AvlTree */
-    private static final byte[] EMPTY_TREE = new byte[1];
+public class ArrayMarshaller<E> implements Marshaller<ArrayTree<E>> {
+	/** used for serialized form of an empty AvlTree */
+	private static final byte[] EMPTY_TREE = new byte[1];
 
-    /** marshaller to be used for marshalling the keys */
-    private Marshaller<E> keyMarshaller;
-    
-    /** key Comparator for the AvlTree */
-    private Comparator<E> comparator;
-    
+	/** marshaller to be used for marshalling the keys */
+	private Marshaller<E> keyMarshaller;
 
-    /**
-     * Creates a new instance of AvlTreeMarshaller with a custom key
-     * Marshaller.
-     *
-     * @param comparator Comparator to be used for key comparision
-     * @param keyMarshaller marshaller for keys
-     */
-    public ArrayMarshaller( Comparator<E> comparator, Marshaller<E> keyMarshaller )
-    {
-        this.comparator = comparator;
-        this.keyMarshaller = keyMarshaller;
-    }
+	/** key Comparator for the AvlTree */
+	private Comparator<E> comparator;
 
+	/**
+	 * Creates a new instance of AvlTreeMarshaller with a custom key Marshaller.
+	 *
+	 * @param comparator    Comparator to be used for key comparision
+	 * @param keyMarshaller marshaller for keys
+	 */
+	public ArrayMarshaller(Comparator<E> comparator, Marshaller<E> keyMarshaller) {
+		this.comparator = comparator;
+		this.keyMarshaller = keyMarshaller;
+	}
 
-    /**
-     * Creates a new instance of AvlTreeMarshaller with the default key
-     * Marshaller which uses Java Serialization.
-     *
-     * @param comparator Comparator to be used for key comparision
-     */
-    public ArrayMarshaller( Comparator<E> comparator )
-    {
-        this.comparator = comparator;
-        this.keyMarshaller = DefaultMarshaller.INSTANCE;
-    }
+	/**
+	 * Creates a new instance of AvlTreeMarshaller with the default key Marshaller which uses Java Serialization.
+	 *
+	 * @param comparator Comparator to be used for key comparision
+	 */
+	public ArrayMarshaller(Comparator<E> comparator) {
+		this.comparator = comparator;
+		this.keyMarshaller = DefaultMarshaller.INSTANCE;
+	}
 
+	@Override
+	public byte[] serialize(ArrayTree<E> tree) {
+		if ((tree == null) || (tree.size() == 0)) {
+			return EMPTY_TREE;
+		}
 
-    /**
-     * Marshals the given tree to bytes
-     * @param tree the tree to be marshalled
-     */
-    public byte[] serialize( ArrayTree<E> tree )
-    {
-        if ( ( tree == null ) || ( tree.size() == 0 ) )
-        {
-            return EMPTY_TREE;
-        }
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(byteStream);
+		byte[] data = null;
 
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        DataOutputStream out = new DataOutputStream( byteStream );
-        byte[] data = null;
-        
-        try
-        {
-            out.writeByte( 0 ); // represents the start of an Array byte stream
-            out.writeInt( tree.size() );
+		try {
+			out.writeByte(0); // represents the start of an Array byte stream
+			out.writeInt(tree.size());
 
-            for ( int position = 0; position < tree.size(); position++ )
-            {
-                E value = tree.get( position );
-                byte[] bytes = keyMarshaller.serialize( value );
-                
-                // Write the key length
-                out.writeInt( bytes.length );
-                
-                // Write the key if its length is not null
-                if ( bytes.length != 0 )
-                {
-                    out.write( bytes );
-                }
-            }
-            
-            out.flush();
-            data = byteStream.toByteArray();
-            
-            // Try to deserialize, just to see
-            try
-            {
-                deserialize( data );
-            }
-            catch (NullPointerException npe )
-            {
-                System.out.println( "Bad serialization, tree : [" + StringTools.dumpBytes( data ) + "]");
-                throw npe;
-            }
+			for (int position = 0; position < tree.size(); position++) {
+				E value = tree.get(position);
+				byte[] bytes = keyMarshaller.serialize(value);
 
-            out.close();
-        }
-        catch( IOException e )
-        {
-            e.printStackTrace();
-        }
-        
-        return data;
-    }
+				// Write the key length
+				out.writeInt(bytes.length);
 
-    
-    /**
-     * Creates an Array from given bytes of data.
-     * 
-     * @param data byte array to be converted into an array  
-     */
-    public ArrayTree<E> deserialize( byte[] data ) throws IOException
-    {
-        try
-        {
-            if ( ( data == null ) || ( data.length == 0 ) )
-            {
-                throw new IOException( "Null or empty data array is invalid." );
-            }
-    
-            if ( ( data.length == 1 ) && ( data[0] == 0 ) )
-            {
-                E[] array = (E[])new Object[]{};
-                return new ArrayTree<>( comparator, array );
-            }
-    
-            ByteArrayInputStream bin = new ByteArrayInputStream( data );
-            DataInputStream din = new DataInputStream( bin );
-            
-            byte startByte = din.readByte();
-            
-            if( startByte != 0 )
-            {
-                throw new IOException("wrong array serialized data format");
-            }
-            
-            int size = din.readInt();
-            E[] nodes = (E[])new Object[size];
-            
-            for ( int i = 0; i < size; i++ )
-            {
-                // Read the object's size
-                int dataSize = din.readInt();
-                
-                if ( dataSize != 0 )
-                {
-                    byte[] bytes = new byte[ dataSize ];
-                    
-                    din.read( bytes );
-                    E key = keyMarshaller.deserialize( bytes );
-                    nodes[i] = key;
-                }
-            }
+				// Write the key if its length is not null
+				if (bytes.length != 0) {
+					out.write(bytes);
+				}
+			}
 
-            return new ArrayTree<>( comparator, nodes );
-        }
-        catch (NullPointerException npe )
-        {
-            System.out.println( "Bad tree : [" + StringTools.dumpBytes( data ) + "]");
-            throw npe;
-        }
-    }
+			out.flush();
+			data = byteStream.toByteArray();
+
+			// Try to deserialize, just to see
+			try {
+				deserialize(data);
+			} catch (NullPointerException npe) {
+				System.out.println("Bad serialization, tree : [" + StringTools.dumpBytes(data) + "]");
+				throw npe;
+			}
+
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return data;
+	}
+
+	@Override
+	public ArrayTree<E> deserialize(byte[] data) throws IOException {
+		try {
+			if ((data == null) || (data.length == 0)) {
+				throw new IOException("Null or empty data array is invalid.");
+			}
+
+			if ((data.length == 1) && (data[0] == 0)) {
+				E[] array = (E[]) new Object[] {};
+				return new ArrayTree<>(comparator, array);
+			}
+
+			ByteArrayInputStream bin = new ByteArrayInputStream(data);
+			DataInputStream din = new DataInputStream(bin);
+
+			byte startByte = din.readByte();
+
+			if (startByte != 0) {
+				throw new IOException("wrong array serialized data format");
+			}
+
+			int size = din.readInt();
+			E[] nodes = (E[]) new Object[size];
+
+			for (int i = 0; i < size; i++) {
+				// Read the object's size
+				int dataSize = din.readInt();
+
+				if (dataSize != 0) {
+					byte[] bytes = new byte[dataSize];
+
+					din.read(bytes);
+					E key = keyMarshaller.deserialize(bytes);
+					nodes[i] = key;
+				}
+			}
+
+			return new ArrayTree<>(comparator, nodes);
+		} catch (NullPointerException npe) {
+			System.out.println("Bad tree : [" + StringTools.dumpBytes(data) + "]");
+			throw npe;
+		}
+	}
 }
