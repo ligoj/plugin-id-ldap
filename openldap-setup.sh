0e7fb5e6d7fcc5f1c7a12d309a503c204aaefac1
Fabrice Daugan <fabrice.daugan@gmail.com>
2024-02-25 17:34:33 +0100

    Cleanup LDAP setup

--- openldap-setup.sh
+++ openldap-setup.sh
@@ -14,27 +14,3 @@ podman run --name openldap \
   -p 1389:1389 \
   bitnami/openldap:latest
 
-bash -c '
-cd /Users/fabdouglas/git/ligoj-plugins/plugin-id-ldap && \
-ldapadd -c -x -D "cn=Manager,dc=sample,dc=com" -w "$LDAP_ADMIN_PASSWORD" -H ldap://localhost:1389 -f  ../plugin-id-ldap-embedded/src/main/resources/export/base.ldif && \
-ldapsearch -c -x -D "cn=Manager,dc=sample,dc=com" -w "$LDAP_ADMIN_PASSWORD" -H ldap://localhost:1389 -b "dc=sample,dc=com"
-'
-
-# Create 100K users
-NB_USER=100000
-source ./.secrets.sh
-rm -f bench.ldiff
-for ((i=1;i<=$NB_USER;i++)); do 
-    echo "Create user $i"
-    echo "
-dn: uid=cli${i},ou=department1,ou=internal,ou=people,dc=sample,dc=com
-cn: Cli${i} Name${i}
-sn: Name${i}
-givenName: Cli${i}
-uid: cli${i}
-mail: cli${i}.name@sample.com
-objectClass: inetOrgPerson
-userPassword: ligoj-user
-" >> bench.ldiff
-done
-ldapadd -c -x -D "cn=Manager,dc=sample,dc=com" -w "$LDAP_ADMIN_PASSWORD" -H ldap://localhost:1389 -f bench.ldiff
