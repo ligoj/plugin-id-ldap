051bdd9589bbb72009a702e6e4ded009049c4223
Fabrice Daugan <fabrice.daugan@gmail.com>
2025-11-08 10:48:55 +0100

    imp: reduce auto-proxy warning

--- src/main/java/org/ligoj/app/plugin/id/ldap/resource/IdLdapCache.java
+++ src/main/java/org/ligoj/app/plugin/id/ldap/resource/IdLdapCache.java
@@ -6,6 +6,8 @@ package org.ligoj.app.plugin.id.ldap.resource;
 import com.hazelcast.cache.HazelcastCacheManager;
 import com.hazelcast.config.CacheConfig;
 import org.ligoj.bootstrap.resource.system.cache.CacheManagerAware;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
@@ -14,6 +16,7 @@ import java.util.function.Function;
  * Cache configuration for LDAP.
  */
 @Component
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class IdLdapCache implements CacheManagerAware {
 
 	@Override
